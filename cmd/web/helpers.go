93691656f5c6ca4e20a088aba0a04cbc59bc5453
Aki <120ce066@gmail.com>
2024-02-24 13:43:35 +0900

    add go-playground for form parsing and validation helper

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/go-playground/form/v4"
 )
 
 // for 500 internal error
@@ -55,4 +58,25 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
 	}
+}
+
+// panic when error is InvalidDecoderError
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		var invalidDecoderError *form.InvalidDecoderError
+
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module snippetbox.hogeisfuga.net
 
 go 1.21.3
 
-require github.com/go-sql-driver/mysql v1.7.1
-
-require github.com/justinas/alice v1.2.0
-
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/justinas/alice v1.2.0
+	github.com/go-playground/form/v4 v4.2.1
+	github.com/julienschmidt/httprouter v1.3.0
+)
